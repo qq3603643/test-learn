44091943b26b46b16ae6853db78b33eb7f630d61
qq3603643 <3603643@qq.com>
2016-05-20 08:05:19 +0800

    在家2016-0519

--- confirmGoods/js/index.js
+++ confirmGoods/js/index.js
@@ -2,16 +2,26 @@ $(function(){
 
 var confirmGoods = function(){
 	var  $calcBox = $('.calcTotal'),
+		 timerHideBottom = null,
 		 confirm = {
 		 	keepBottom: function(){
+		 		clearTimeout(timerHideBottom);
+		 		$calcBox.css({'visibility':'hidden','opacity':'0'});
 		 		var calcBoxHeight = $calcBox.height(),scrollTop = $(window).scrollTop(),
 		 			itop = $(window).height()-calcBoxHeight+scrollTop;
-
-		 		$calcBox.css({'top':itop})
+		 		    $calcBox.css({'top':itop});
+	 		    
+	 		    timerHideBottom = setTimeout(function(){ $calcBox.animate({
+	 		    	'opacity':'1',
+	 		    	'visibility':'visible'
+	 		    }),400},400)
 		 	},
 		 	run: function(){
 		 		confirm.keepBottom();
-		 		$(window).on('scroll',confirm.keepBottom)
+		 		$(window).on({
+		 			'scroll':confirm.keepBottom,
+		 			'resize':confirm.keepBottom
+		 		});
 		 	}
 		 };
 
